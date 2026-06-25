69b6877b09e86e2f9006790e4ff72e2d50055d0f
JudeTulel <judeyego202@gmail.com>
2026-06-25 15:53:57 +0300

    feat: wallet Connection

---
+++ context/appkit.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { createAppKit } from "@reown/appkit/react";
+import { EthersAdapter } from "@reown/appkit-adapter-ethers";
+import { mainnet, arbitrum } from "@reown/appkit/networks";
+import { type ReactNode } from "react";
+
+const projectId = process.env.PROJECT_ID!
+
+const metadata = {
+  name: "My Website",
+  description: "My Website description",
+  url: "https://mywebsite.com",
+  icons: ["https://avatars.mywebsite.com/"],
+};
+
+createAppKit({
+  adapters: [new EthersAdapter()],
+  metadata,
+  networks: [mainnet, arbitrum],
+  projectId,
+  features: {
+    analytics: true,
+    socials:['x','google'],
+    email:true,
+    
+
+  },
+});
+
+export function AppKit({ children }: { children: ReactNode }) {
+  return <>{children}</>;
+}
